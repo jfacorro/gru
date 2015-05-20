a873e8225266ff7eae69b819bb54933aedfc7729
Juan Facorro <juan.facorro@gmail.com>
2015-05-20 00:30:20 -0300

    [#14] Ignore start requests while already starting client

--- lib/grog/client/server.ex
+++ lib/grog/client/server.ex
@@ -3,38 +3,45 @@ defmodule Grog.Client.Server do
   alias Grog.Utils
 
   def start(client, n, rate) do
-    GenServer.cast(__MODULE__, {:start, client, n, rate})
+    GenServer.call(__MODULE__, {:start, client, n, rate})
   end
 
   def stop do
-    GenServer.cast(__MODULE__, :stop)
+    GenServer.call(__MODULE__, :stop)
   end
 
   ## GenServer
 
   def start_link do
-    GenServer.start_link(__MODULE__, [], [name: __MODULE__])
+    GenServer.start_link(__MODULE__, nil, [name: __MODULE__])
   end
 
-  def handle_cast({:start, client, n, rate}, _) do
-    state = %{client: client, n: n, rate: rate}
-    {:noreply, state, 0}
+  def handle_call({:start, client, n, rate}, _from, nil) do
+    state = %{client: client, n: n, rate: rate, now: nil}
+    {:reply, :ok, state, 0}
   end
-  def handle_cast(:stop, state) do
-    Grog.Client.Supervisor.children()
+  def handle_call({:start, _, _, _}, _from, state) do
+    diff = :timer.now_diff(:os.timestamp, state.now)
+    timeout = round(1000 - diff / 1000)
+    {:reply, :busy, state, timeout}
+  end
+  def handle_call(:stop, _from, _state) do
+    count = Grog.Client.Supervisor.count
+    Grog.Client.Supervisor.children
     |> Enum.map(&Grog.Client.Supervisor.stop_child/1)
 
-    {:noreply, state}
+    {:reply, {:ok, count}, nil}
   end
 
   def handle_info(:timeout, %{n: 0}) do
     {:noreply, nil}
   end
   def handle_info(:timeout, state) do
+    IO.inspect(state)
     Utils.repeat(state.client, min(state.rate, state.n))
     |> Enum.map(&Grog.Client.Supervisor.start_child/1)
 
-    state = %{state | n: max(0, state.n - state.rate)}
+    state = %{state | n: max(0, state.n - state.rate), now: :os.timestamp()}
     {:noreply, state, 1000}
   end
 end
