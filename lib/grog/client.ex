b080a7a633e79f94844a4194e3658e927a1adad6
Juan Facorro <juan.facorro@gmail.com>
2015-05-25 18:28:21 -0300

    [#22] Split Grog.Client into Grog.Client and Grog.Client.Tasks

--- lib/grog/client.ex
+++ lib/grog/client.ex
@@ -18,10 +18,12 @@ defmodule Grog.Client do
   end
 
   def handle_info(:timeout, state) do
-    tasks = state.module.__tasks__()
-    n = Utils.uniform(length(tasks))
-    task_name = Enum.at(tasks, n)
-    apply(state.module, task_name, [state.client])
+    tasks = state.client.tasks_module.__tasks__()
+    if tasks != [] do
+      n = Utils.uniform(length(tasks))
+      task_name = Enum.at(tasks, n)
+      apply(state.client.tasks_module, task_name, [state.client])
+    end
 
     {:noreply, state, wait_timeout(state)}
   end
@@ -34,11 +36,6 @@ defmodule Grog.Client do
 
   defmacro __using__(opts) do
     quote do
-      import unquote(__MODULE__), only: [deftask: 2]
-      @before_compile Grog.Client
-      # Accumulate tasks with their weights
-      Module.register_attribute(__MODULE__, :tasks, accumulate: true)
-
       def __init__ do
         data = Dict.merge(%{}, unquote(opts))
         init(data)
@@ -50,27 +47,6 @@ defmodule Grog.Client do
 
       defoverridable [init: 1, terminate: 1]
     end
-  end
-
-  defmacro __before_compile__(_env) do
-    tasks = Module.get_attribute(__CALLER__.module, :tasks)
-            |> Enum.flat_map( fn {name, weight} -> Grog.Utils.repeat(name, weight) end)
-            |> Enum.reverse
-    quote do
-      def __tasks__, do: unquote(tasks)
-    end
-  end
-
-  defmacro deftask(definition = {name, _, _}, do: contents) do
-    quote do
-      Grog.Client.__on_definition__(__ENV__, unquote(name))
-      def unquote(definition), do: unquote(contents)
-    end
-  end
-
-  def __on_definition__(env, name) do
-    weights = Module.get_attribute(env.module, :weight) || 1
-    Module.put_attribute(env.module, :tasks, {name, weights})
   end
 
   ## Internal functions
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,14 +1,19 @@
 ExUnit.start()
 
-defmodule GrogTest.Client do
-  use Grog.Client, name: "Test", min_wait: 500, max_wait: 1000
+defmodule GrogTest.Client.Tasks do
+  use Grog.Client.Tasks
   alias Grog.Metric.Server
   alias Grog.Metric.Count
-  require Logger
 
   @weight 100
   deftask get_status(data) do
     Server.report(%Count{name: "Test"}, 1)
     data
   end
+end
+
+defmodule GrogTest.Client do
+  use Grog.Client, name: "Test Client",
+                   min_wait: 500, max_wait: 1000,
+                   tasks_module: GrogTest.Client.Tasks
 end
