14e0d64c7fc477768893a8402d5ca7d0f9f09d1a
Juan Facorro <juan.facorro@gmail.com>
2015-05-31 18:49:32 -0300

    [#5] Add content-type to static content

--- lib/grog/web/utils.ex
+++ lib/grog/web/utils.ex
@@ -5,12 +5,21 @@ defmodule Grog.Web.Utils do
     expr = quote do
       "/" <> Enum.join(var!(conn).path_info, "/")
     end
+
     quote do
       get unquote(at) do
         path = unquote(root) <> unquote(from || expr)
+        ext = :filename.extension(path) |> String.replace(".", "")
+        mime_types =  %{"js" => "application/javascript",
+                        "css" => "text/css",
+                        "png" => "image/png",
+                        "jpg" => "image/jpg",
+                        "html" => "text/html"}
         case File.read(path) do
           {:ok, content} ->
-            send_resp(var!(conn), 200, content)
+            var!(conn)
+            |> put_resp_header("Content-Type", Map.get(mime_types, ext, "text/plain"))
+            |> send_resp(200, content)
           {:error, :enoent} ->
             send_resp(var!(conn), 500, "Web server not available")
         end
